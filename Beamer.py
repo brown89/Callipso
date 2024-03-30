1dce4fbe35cd3e1da2bd18f9eb8fd16fcba8b3b3
Bjoern Bruun <bjoern@radisurf.com>
2024-03-30 21:42:17 +0100

    Small update

--- Beamer.py
+++ Beamer.py
@@ -14,10 +14,14 @@ def area_ellise(radius:float, grazing_angle:float) -> float:
     return np.pi * radius**2 / np.cos(np.deg2rad(grazing_angle))
 
 
-if __name__ == '__main__':
+def test():
     angles = [0, 25, 35, 45, 55, 65, 75, 80, 85, 89]
 
     for angle in angles:
         area = area_ellise(1, angle)
 
         print(f"Angle: {angle:.1f}, area: {area:.1f}")
+
+
+if __name__ == '__main__':
+    test()
