1a3919dad9aab488f3f412129ef361fb2829a6d6
Bjoern Bruun <bjoern@radisurf.com>
2024-04-30 10:17:45 +0200

    Added dynamic data start clause

--- Modules/JAW.py
+++ Modules/JAW.py
@@ -2,13 +2,30 @@ import os
 import re
 import pandas as pd
 
-SUPPORTED_FILE_EXTENSIONS = ['*.txt']
 
+# Ellipsometer specific constants
 BEAM_SIZE_WITH_FOCUS_PROBES = 0.03
 BEAM_SIZE_WITHOUT_FOCUS_PROBES = 0.3
 
+SUPPORTED_FILE_EXTENSIONS = ['*.txt']
 
-def is_valid(filename):
+DATA_HEAD_NAMES = {
+    'Point #': 'n_points',
+    'Z Align': 'z_align',
+    'SigInt': 'sig_int',
+    'Tilt X': 'tilt_x',
+    'Tilt Y': 'tilt_y',
+    'Hardware OK': 'hardware_ok',
+    'MSE': 'mse',
+    'Thickness # 1 (nm)': 'thickness_nm',
+    'A': 'a',
+    'B': 'b',
+    'C': 'c',
+    'Fit OK': 'fit_ok', 
+}
+
+
+def is_valid(filename:str) -> bool:
     """
     Function for validating file. Can raise one of two errors:
     
@@ -17,21 +34,50 @@ def is_valid(filename):
     ValueError if file type not supported
     """
     
-    # Check if 'file_path' is valid
+    # Check if 'filename' is valid
     if not os.path.exists(filename):
         raise FileNotFoundError(f"Could not find file:\n{filename}")
     
     # Check for correct file extension
     _, file_extension = os.path.splitext(filename)
-    if file_extension not in '\t'.join(SUPPORTED_FILE_EXTENSIONS):
+    if file_extension not in ', '.join(SUPPORTED_FILE_EXTENSIONS):
         raise ValueError(f"Unsupported file type, supported files are; {SUPPORTED_FILE_EXTENSIONS}, were given; {file_extension}.")
     
     return True
 
 
-def read_text_file(filename):
-    # Read in file to DataFrame
-    data = pd.read_csv(filename, sep="\t", header=0, skiprows=range(1, 7))
+def first_line_of_data(filename:str, match_pattern:str) -> int:
+    """
+    Finds the line where data begins."""
+    # Read file line by line
+    with open(filename, 'r') as f:
+        file = f.readlines()
+    
+    # Loops through lines in file
+    start_of_data: int
+    for i, line in enumerate(file):
+
+        # Stops if first character is a match for 'match_pattern'
+        if line[0] == match_pattern:
+            i = start_of_data
+            break
+    
+    return start_of_data
+    
+
+
+def read_text_file(filename:str) -> pd.DataFrame:
+    """
+    Read file and extracts x and y coordinates
+
+    Returns a DataFrame
+    """
+
+    # Find where data starts
+    start_of_data = first_line_of_data(filename, '(')
+
+    # Read file into DataFrame
+    data = pd.read_csv(filename, sep="\t", header=0, skiprows=range(1, start_of_data))
     
     # Add x and y column
     x_list, y_list = [], []
@@ -41,30 +87,24 @@ def read_text_file(filename):
         x_list.append(float(x))
         y_list.append(float(y))
 
+    # Setting new columns with x and y values
     data['x'] = x_list
     data['y'] = y_list
 
-    # Drops 1st column
+    # Drops 1st column with old (x, y) coordinates
     data.drop(columns=data.columns[0], axis=1,  inplace=True)
 
-    # Rename all columns
-    new_names = {name: name.strip() for name in data.columns}
-    data.rename(columns=new_names, inplace=True)
-
-    print("Column names:")
-    for col_name in new_names:
-        print(f"- {col_name}")
-    
     return data
 
+
+
 class JAW:
-    def __init__(self, file_path: str):
+    def __init__(self, filename: str):
 
-        is_valid(file_path)  # Validate file
+        is_valid(filename)  # Validate file
 
-        self.file_path = file_path
-        self.name = os.path.basename(file_path)
-        self.data = read_text_file(file_path)
+        self.filename = filename
+        self.name = os.path.basename(filename)
 
-
-
+        data = read_text_file(filename)
+        self.data = data.rename(DATA_HEAD_NAMES)
